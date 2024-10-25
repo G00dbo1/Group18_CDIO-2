7b44feeaa81193222a5aff8b526ae267ab9a2dcc
Viktor <viktorthysen@gmail.com>
2024-10-25 14:33:01 +0200

    made the account class and a object that is called to update the ammount in the class, as well as making sure the update couldnt cause the balance to reach a negative ammount

--- src/Account.java
+++ src/Account.java
@@ -1,5 +1,16 @@
 package src;
 
 public class Account {
+    int balance;
     
+    Account(int coins){
+        this.balance = coins;
+    }
+
+    Void updateCoin(int change){
+        this.balance = this.balance + change; 
+        if (this.balance + a < 0){
+            this.balance = 0;
+        }
+    }
 }
