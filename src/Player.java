ce6e3bc8deedf76dc6d09390f9bc849a29c28682
Razi <s230465@dtu.dk>
2024-10-29 22:20:44 +0100

    Created Player class - added player class to manage player position within range validation - including "getPosition" to retrieve position and "updatePosition" to update current placement.

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,18 @@
 package src;
-
+// Player's position
 public class Player {
-
+    private int position;
+    // Start's player in position 1
+    public Player(int position = 1;) {
+        this.position = position;
+    }
+    // Pull's the information about the current position
+    public int getPosition() {
+        return this.position;
+    }
+    // Updates the position within 1-12 range
+    public void updatePosition(int dice){
+        this.position = ((this.position + dice - 1) % 12) + 1;
+    }
 }
+
