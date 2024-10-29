6749f3e3857627073cca981c8a4130cbca3c26ec
LuskeRuby <rubyman2@gmail.com>
2024-10-29 15:25:30 +0100

    updated the methods for rolling dices

--- src/Die.java
+++ src/Die.java
@@ -3,19 +3,23 @@ package src;
 import java.util.Random;
 
 public class Die {
-    private int faces;
+    private int faceValue;
+    private int amountFaces;
     private Random random;
     
      // Constructor
-    public Dice(int facesDie1, int facesDie2) {
-        this.facesDie1 = facesDie1;
-        this.facesDie2 = facesDie2;
+    public Die(int amountFaces) {
         this.random = new Random();
+        this.amountFaces = amountFaces;
+        this.faceValue = random.nextInt(amountFaces) + 1;
     }
 
     public int getFaceValue() {
-        int rollDie1 = random.nextInt(facesDie1) + 1;
-        int rollDie2 = random.nextInt(facesDie2) + 1;
-        return rollDie1 + rollDie2;
+        return this.faceValue;
     }
+
+    public void roll() {
+        this.faceValue = random.nextInt(amountFaces) + 1;
+    }
+
 }
